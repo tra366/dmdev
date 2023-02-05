3afc8c455768d488501b1037bf57cbda8e4afefc
Turusikov Roman <tra366@yandex.ru>
2023-02-06 00:55:44 +0300

    apache-maven

---
+++ my-maven/common/src/test/java/integration/ITGeneral.java
@@ -0,0 +1,14 @@
+package integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ITGeneral {
+
+    @Test
+    void myIT(){
+        assertEquals(1,1);
+    }
+
+
+}
---
+++ my-maven/service/src/main/java/com/dmdev/service/service/UserService.java
@@ -0,0 +1,12 @@
+package com.dmdev.service.service;
+
+import com.dmdev.General;
+import com.dmdev.service.dto.UserDto;
+
+public class UserService {
+
+    private General general = new General();
+    public UserDto getUser(String name){
+        return null;
+    }
+}
